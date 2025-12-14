8d2c6e50a4022a61c1711427311df3d07e9e5076
Elisson Marques <nadsonnodachi@gmail.com>
2025-12-14 01:42:22 +0000

    feat(auth): force account selection in google strategy

--- src/modules/auth/strategies/google.strategy.ts
+++ src/modules/auth/strategies/google.strategy.ts
@@ -16,10 +16,11 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor() {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
-      scope: ['email', 'profile'],
-    });
+      authorizationParams: {
+        prompt: 'select_account',
+        access_type: 'offline',
+      },
+    } as any);
   }
 
   async validate(
